9e7fb748fe86fd5c1925c6019d4f819a0a7bee32
Gregory Varnum <gvarnum@wikimedia.org>
2020-05-12 01:08:26 -0400

    Original beta version 0.12052020
    Change-Id: I2d4dce2fc1bcab6669c49753fac7f965e0085914

--- WikiShare.body.php
+++ WikiShare.body.php
@@ -1,135 +1,184 @@
 <?php
-if (!defined('MEDIAWIKI')) die();
+
 /**
  * Class file for the WikiShare extension
  *
- * @addtogroup Extensions
- * @license GPL
+ * @wikisharegroup Extensions
+ * @license GPL-2.0-only
  */
 class WikiShare {
 
 	/**
-	 * Register parser hook
+	 * Respond to the parser hook call to invoke the 'wikishareserv' magic word
 	 *
-	 * @param $parser Parser
-	 * @return bool
+	 * @param Parser $parser
 	 */
-	public static function WikiShareHeaderTag( &$parser ) {
-		$parser->setHook( 'wikishare', __CLASS__.'::parserHook' );
-		return true;
+	public static function onParserFirstCallInit( Parser $parser ) {
+		$parser->setFunctionHook( 'wikishareserv', [ self::class, 'doParserFunction' ] );
+		$parser->setHook( 'wikishare', [ self::class, 'doParserHook' ] );
+	}
+
+	/**
+	 * Add properties to the page based on the parser function details.
+	 *
+	 * @param Parser $parser
+	 * @return string
+	 */
+	public static function doParserFunction( Parser $parser, $twitter = '' ) {
+		$parser->getOutput()->setExtensionData( 'wikishareserv:twitter', $twitter );
+		return '';
 	}
 
 	/**
 	 * Parser hook for the <wikishare /> tag extension.
 	 *
-	 * @param $parser
+	 * @param Parser $parser
 	 * @return string
 	 */
-	 static function parserHook( $parser ) {
-		global $wgWikiShare, $wgWikiSharepubid, $wgWikiShareHServ, $wgWikiShareBackground, $wgWikiShareBorder;
+	public static function doParserHook( $input, array $args, Parser $parser ) {
+		global $wgWikiShare, $wgWikiShareServices;
 
-		# Load css stylesheet
-		$wgOut->addModuleStyles( 'ext.wikiShare.main' );
+		# Localisation for "Share"
+		$share = wfMessage( 'wikishare' )->escaped();
 
 		# Output WikiShare widget
-		$output ='<!-- WikiShare Button BEGIN -->
-			<div id="wikisharetoolbar" style="background:'.$wgWikiShareBackground.'; border-color:'.$wgWikiShareBorder.';">';
+		$output = '<!-- WikiShare Buttons BEGIN -->
+			<div class="wikishare_toolbox wikishare_default_style" id="wikisharetoolbar" style="background:' .
+			$wgWikiShare['background'] . '; border-color:' . $wgWikiShare['border'] . ';">';
 
-		$output .= self::makeLinks( $wgWikiShareHServ );
-
-        $output .='</div>';
+		$output .= self::makeLinks( $wgWikiShareServices, $parser );
+		$output .= '</div>';
 
 		return $output;
 	}
 
-/**
- * Function to setup article URL and title for service URLs
- *
- * @param $article
- * @return bool
- */ 
-    
-
-
 	/**
-	 * Function for article header toolbar
+	 * Function for article header share toolbar
 	 *
-	 * @param $article Article
-	 * @param $outputDone
-	 * @param $pcache
+	 * @param Article &$article
+	 * @param bool &$outputDone
+	 * @param bool &$pcache
 	 * @return bool|bool
 	 */
 	public static function WikiShareHeader( &$article, &$outputDone, &$pcache ) {
-		global $wgOut, $wgWikiShare, $wgWikiShareHeader, $wgWikiShareMain,
-		       $wgWikiShareHServ, $wgWikiShareBackground, $wgWikiShareBorder;
+		global $wgOut, $wgWikiShare, $wgWikiShareServices;
 
-		# Check if page is in content namespace and the setting to enable/disable article header tooblar either on the main page or at all
+		# Check if page is in content namespace and the setting to enable/disable
+		# article header tooblar either on the main page or at all
 		if ( !MWNamespace::isContent( $article->getTitle()->getNamespace() )
-			|| !$wgWikiShareHeader
-			|| ( $article->getTitle()->equals( Title::newMainPage() ) && !$wgWikiShareMain )
+			|| !$wgWikiShare['header']
+			|| ( $article->getTitle()->equals( Title::newMainPage() ) && !$wgWikiShare['main'] )
 		) {
 			return true;
 		}
-        
-		# Load css stylesheet
-		$wgOut->addModuleStyles( 'ext.wikiShare.main' );
+
+		# Localisation for "Share"
+		$share = wfMessage( 'wikishare' )->escaped();
 
 		# Output WikiShare widget
-		$wgOut->addHTML('<!-- WikiShare Button BEGIN -->
-			<div id="wikisharetoolbar" style="background:'.$wgWikiShareBackground.'; border-color:'.$wgWikiShareBorder.';">');
-		
-        $wgOut->addHTML( self::makeLinks( $wgWikiShareHServ ) );
-        
-		$wgOut->addHTML('</div>');
+		$wgOut->addHTML( '<!-- WikiShare Buttons BEGIN -->
+			<div class="wikishare_toolbox wikishare_default_style" id="wikisharetoolbar" style="background:' .
+			$wgWikiShare['background'] . '; border-color:' . $wgWikiShare['border'] . ';">' );
+
+		$wgOut->addHTML( self::makeLinks( $wgWikiShareServices ) );
+
+		$wgOut->addHTML( '</div>' );
 
 		return true;
 	}
 
 	/**
-	 * Function for sidebar portlet
+	 * Function for share sidebar portlet
 	 *
-	 * @param $skin
-	 * @param $bar
-	 * @return bool|array|bool
+	 * @param Skin $skin
+	 * @param Sidebar &$bar
+	 * @return bool|array
 	 */
 	public static function WikiShareSidebar( $skin, &$bar ) {
-		global $wgOut, $wgWikiShare, $wgWikiShareSidebar, $wgWikiShareSBServ;
+		global $wgOut, $wgWikiShare, $wgWikiShareServices;
+
+		# Load css stylesheet
+		$wgOut->addModuleStyles( 'ext.WikiShare' );
 
 		# Check setting to enable/disable sidebar portlet
-		if ( !$wgWikiShareSidebar ) {
+		if ( !$wgWikiShare['sidebar'] ) {
 			return true;
 		}
 
-		# Localisation for "Share"
-		$share = wfMessage( 'wikishare-share' )->escaped();
-        
-		# Load css stylesheet
-		$wgOut->addModuleStyles( 'ext.wikiShare.main' );
-
 		# Output WikiShare widget
-		$bar['wikishare-share'] = '<!-- WikiShare Button BEGIN -->
-			<div id="wikisharesidebar">';
+		$bar['wikishare'] = '<!-- WikiShare Buttons BEGIN -->
+			<div class="wikishare_toolbox wikishare_default_style" id="wikisharesidebar">';
 
-		$bar['wikishare-share'] .= self::makeLinks( $wgWikiShareSBServ );
+		$bar['wikishare'] .= self::makeLinks( $wgWikiShareServices );
 
-		$bar['wikishare-share'] .= '</div>';
+		$bar['wikishare'] .= '</div>';
 
 		return true;
-	}    
+	}
+
+	/**
+	 * Function for follow sidebar portlet
+	 *
+	 * @param Skin $skin
+	 * @param Sidebar &$bar
+	 * @return bool|array
+	 */
+	public static function WikiShareFollowSidebar( $skin, &$bar ) {
+		global $wgOut, $wgWikiShare, $wgWikiShareFollow;
+
+		# Load css stylesheet
+		$wgOut->addModuleStyles( 'ext.WikiShare' );
+
+		# Check setting to enable/disable sidebar portlet
+		if ( !$wgWikiShare['follow'] ) {
+			return true;
+		}
+
+		# Output WikiShare widget
+		$bar['wikishare-follow'] = '<!-- WikiShare Buttons BEGIN -->
+			<div class="wikishare_follow" id="wikisharefollow">';
+
+		$bar['wikishare-follow'] .= self::makeLinks( $wgWikiShareFollow );
+
+		$bar['wikishare-follow'] .= '</div>';
+
+		return true;
+	}
 
 	/**
 	 * Converts an array definition of links into HTML tags
 	 *
-	 * @param $links array
+	 * @param array $links
 	 * @return string
 	 */
-    protected static function makeLinks( $links ) {
-		global $wgOut;
-        $articlefullurl = urlencode( $wgOut->getTitle()->getFullURL() );
-		
-        $html = '';
-		foreach ( $links as $link ) {
-			$html .= '<a class="wikishare_button wikishare_button_' . $link['service'] . '" href="' . $link['url'] . '' . $articlefullurl .'" title="Share on ' . $link['service'] . '"><img alt="Share on ' . $link['service'] . '" src="' . $link['image'] . '"></a>';
+	protected static function makeLinks( $links, $parser = null ) {
+		global $wgOut, $wgSitename;
+
+		if ( $parser ) {
+			$pout = $parser->getOutput();
+		} else if ( $wgOut->canUseWikiPage() ) {
+			$wikiPage = $wgOut->getWikiPage();
+			$pout = $wikiPage->getParserOutput( $wikiPage->makeParserOptions( 'canonical' ) );
+		}
+
+		$html = '';
+		$twitter = '';
+		$title = $wgOut->getPageTitle();
+		$twitter = $pout ? $pout->getExtensionData( 'wikishareserv:twitter' ) : null;
+		if ( !$twitter ) {
+		  $twitter = $wgOut->getPageTitle();
+		}
+		if ( $wgOut->canUseWikiPage() ) {
+			foreach ( $links as $link ) {
+				$attribs = $link['attribs'] ?? '';
+				$pageurl = $wgOut->getTitle()->getFullURL();
+				$servurl = str_replace("%url%", $pageurl, $link['url']);
+				$servurl = str_replace("%title%", $title, $servurl);
+				$servurl = str_replace("%twitter%", $twitter, $servurl);
+				$servurl = str_replace("%wiki%", $wgSitename, $servurl);
+
+				$html .= '<span title="Share on ' . $link['service'] . '"><a class="wikishare_button_' . $link['service'] . '" ' . $attribs . ' href="' . $servurl . '" target="_blank"><img class="wikishare_icon" src="' . $link['icon'] . '" width="20px"></a></span>';
+			}
 		}
 
 		return $html;
